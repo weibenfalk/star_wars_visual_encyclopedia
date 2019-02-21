a3341a9419058043a71b58a4c9396fe347a714dd
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2019-02-21 11:35:27 +0100

    Fixed nav

--- src/components/widgets/Navigation/Navigation.js
+++ src/components/widgets/Navigation/Navigation.js
@@ -3,55 +3,27 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Navigation.css";
 
-const Navigation = ({ category, element }) => {
-  const test = "test";
-  const getHistory = () => {
-    let history = "";
-    let cat = "";
-    let title = "";
-    let home = (
-      <span className="nav-item">
-        <Link to="/">Home</Link>
-        <span className="nav-divider">/</span>
-      </span>
-    );
-
-    if (category) {
-      cat = (
-        <span className="nav-item">
-          <Link to={`/${category}`}>{category}</Link>
-        </span>
-      );
-
-      history = (
-        <span>
-          {home}
-          {cat}
-        </span>
-      );
-    }
-
-    if (element) {
-      title = (
+const Navigation = ({ category, element }) => (
+    <div className="header-navigation">
+      {category &&
+        <>
+          <span className="nav-item">
+            <Link to="/">Home</Link>
+          <span className="nav-divider">/</span>
+          </span>
+          <span className="nav-item">
+            <Link to={`/${category}`}>{category}</Link>
+          </span>
+        </>
+      }
+      {element &&
         <span className="nav-item">
           <span className="nav-divider">/</span>
           {element.name ? element.name : element.title}
         </span>
-      );
-
-      history = (
-        <span>
-          {home}
-          {cat}
-          {title}
-        </span>
-      );
-    }
-    return history;
-  };
-
-  return <div className="header-navigation">{getHistory()}</div>;
-};
+      }
+    </div>
+);
 
 Navigation.propTypes = {
   category: PropTypes.string, // a string with a path like "/example/example"
